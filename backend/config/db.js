522f33112447357a6c9c7774da0bcd410fcf63a9
Ritesh Raj Moond <moondritesh@gmail.com>
2024-09-15 20:37:21 +0530

    db.js for better stability

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 
 const db = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error("caonnection to mongodb failed", err);
