46e7eee844e7edc09e28599e213fa0fe32440016
Ritesh Raj Moond <moondritesh@gmail.com>
2024-09-16 11:50:05 +0530

    updation of vercel.json

--- backend/main.js
+++ backend/main.js
@@ -14,22 +14,25 @@ const chatRoutes = require("./routes/chat-routes");
 const app = express();
 
 // Determine allowed origin based on environment
-const allowedOrigin = process.env.NODE_ENV === 'production' 
-  ? process.env.PRODUCTION_URL 
-  : 'http://localhost:5173';
+const allowedOrigin =
+  process.env.NODE_ENV === "production"
+    ? process.env.PRODUCTION_URL
+    : "http://localhost:5173";
 
 // middlewares
-app.use(cors({
-  origin: allowedOrigin,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // Use Morgan only in development
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
 
@@ -39,6 +42,12 @@ app.use("/api/chat", chatRoutes);
 
 db().then(() => {
   app.listen(process.env.PORT || 9090, () => {
-    console.log(`Server is running on ${process.env.NODE_ENV === 'production' ? 'production URL' : 'http://localhost:9090'}`);
+    console.log(
+      `Server is running on ${
+        process.env.NODE_ENV === "production"
+          ? "production URL"
+          : "http://localhost:9090"
+      }`
+    );
   });
 });
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,7 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL =
-  (import.meta.env.REACT_APP_BASE_URI || "http://localhost:9090") + "/api";
+axios.defaults.baseURL = `${import.meta.env.VITE_REACT_APP_BASE_URI}/api`;
 axios.defaults.withCredentials = true; // This is important for the cookie to be sent to the server
 
 const theme = createTheme({
